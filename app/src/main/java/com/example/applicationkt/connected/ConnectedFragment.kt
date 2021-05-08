04bdba99cf8a12bf4f602d50777c35e22d7455b7
lilian <lilian.pacaud1@gmail.com>
2021-05-08 15:40:45 +0200

    add controls

--- app/src/main/java/com/example/applicationkt/connected/ConnectedFragment.kt
+++ app/src/main/java/com/example/applicationkt/connected/ConnectedFragment.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.VideoView
 import androidx.lifecycle.ViewModelProvider
 import com.example.applicationkt.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,55 +19,51 @@ class ConnectedFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connected)
+        var isLightsON = false
 
-        val lights: Button = findViewById(R.id.lights)
-        val lightsOff: Button = findViewById(R.id.lightsOff)
+        val lights: ImageButton = findViewById(R.id.lights)
+        val forward: ImageButton = findViewById(R.id.arrow_up)
+        val backward: ImageButton = findViewById(R.id.arrow_down)
+        val left: ImageButton = findViewById(R.id.arrow_left)
+        val right: ImageButton = findViewById(R.id.arrow_right)
+        val baseUrl = "https://pentastyle-gull-3744.dataplicity.io"
+        val stream: VideoView = findViewById(R.id.streaming)
 
         lights.setOnClickListener{
-            lightsON("https://pentastyle-gull-3744.dataplicity.io/on")
+            if(isLightsON){
+                CallUrl("${baseUrl}/off")
+                isLightsON = false
+            }
+            else{
+                CallUrl("${baseUrl}/on")
+                isLightsON = true
+            }
         }
 
-        lightsOff.setOnClickListener{
-            lightsOFF("https://pentastyle-gull-3744.dataplicity.io/off")
+        forward.setOnClickListener{
+            CallUrl("${baseUrl}/F")
+        }
+        backward.setOnClickListener{
+            CallUrl("${baseUrl}/B")
+        }
+        left.setOnClickListener{
+            CallUrl("${baseUrl}/L")
+        }
+        right.setOnClickListener{
+            CallUrl("${baseUrl}/R")
         }
 
-        //run("https://pentastyle-gull-3744.dataplicity.io")
-        //lights("https://pentastyle-gull-3744.dataplicity.io/lights")
     }
 
-    fun run(url: String) {
+    fun CallUrl(url: String){
         val request = Request.Builder()
-            .url(url)
-            .build()
+                .url(url)
+                .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {}
             override fun onResponse(call: Call, response: Response): Unit =
-                response.body!!.string().let { Log.v("rasp", it) }
-        })
-    }
-
-    fun lightsON(url: String) {
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response): Unit =
-                response.body!!.string().let { Log.v("lights", it) }
-        })
-    }
-
-    fun lightsOFF(url: String) {
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response): Unit =
-                response.body!!.string().let { Log.v("lightsOff", it) }
+                    response.body!!.string().let { Log.v("request", it) }
         })
     }
 }
