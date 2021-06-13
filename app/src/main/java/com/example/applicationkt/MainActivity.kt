82ffa82b9068dd13c44ea00075ec4173d629ae28
lilian <lilian.pacaud1@gmail.com>
2021-06-13 17:04:16 +0200

    add lottie animation

--- app/src/main/java/com/example/applicationkt/MainActivity.kt
+++ app/src/main/java/com/example/applicationkt/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.applicationkt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.applicationkt.connected.ConnectedFragment
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +20,9 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener{
             val intent = Intent(this, ConnectedFragment::class.java)
             startActivity(intent)
+
         }
+
     }
 
 }
